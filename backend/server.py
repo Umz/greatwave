7fb7081740e8973f0cf28234b6249caccd2bf8ff
Umz <umz@umzgames.com>
2023-12-14 08:33:48 +0000

    Server page running with manual city entries- key removed from push

--- backend/server.py
+++ backend/server.py
@@ -1,10 +1,35 @@
-from flask import Flask
+import requests
+from flask import Flask, jsonify, Response, request
+from flask_cors import CORS
+from dataFormat import getData
 
 app = Flask(__name__)
+CORS(app)
+
+# Load all locations from the spreadsheet and get lon, lat
+
+locations = {
+    "London": {"longitude": -0.1274, "latitude": 51.50853},
+    "New York": {"longitude": -74.00597, "latitude": 40.71427},
+    "Tokyo": {"longitude": 139.69171, "latitude": 35.6895},
+}
 
 @app.route('/', methods=['GET'])
 def hello():
-    return '<h1>Hello, World!</h1>'
 
+    key = '#'
+    city = request.args.get('city', default='London', type=str)
+    lon, lat = locations[city]["longitude"], locations[city]["latitude"]
+    
+    url = f'http://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={key}'
+
+    response = requests.get(url)
+    if response.status_code == 200:
+        json = response.json()
+        data = getData(json)
+        return jsonify(data)
+    else:
+        return Response("{'a':'b'}", status=500, mimetype='application/json')
+    
 if (__name__ == '__main__'):
-    app.run()
+    app.run(debug=True)
